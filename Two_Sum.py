64d0a43644059c99b1d30f0a31677ba1f3192658
Vineet Vinayak <27607402+VVinayak@users.noreply.github.com>
2017-12-26 08:52:42 -0500

    Create Two_Sum.py

---
+++ Two_Sum.py
@@ -0,0 +1,19 @@
+19 / 19 test cases passed.
+Runtime: 618 ms
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        table = {}
+        for i in xrange(len(nums)):
+            table[i] = nums[i]
+            complement =  target - nums[i]
+            if complement in table.values() and table.keys()[table.values().index(complement)] != i:
+                result = [i, table.keys()[table.values().index(complement)]]
+                return result
+                
+             
